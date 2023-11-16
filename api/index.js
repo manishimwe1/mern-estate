d7ea4b160ecc7752cd59f8aab56a57beea12a39c
manishimwe1 <emmymanishimwem@gmail.com>
2023-11-16 12:52:08 +0200

    connect to dataBase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to mongodb!');
+}).catch((error) => {
+    console.log(error);
+})
 
 const app = express()
 

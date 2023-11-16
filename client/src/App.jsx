36f3310af65e7803d8458453fd29118e27ae6e70
manishimwe1 <emmymanishimwem@gmail.com>
2023-11-16 10:54:25 +0200

    add route fanctionarity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+
 
 export const App = () => {
-  return (
-    <div className=' bg-red-500'>App</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home/> } />
+      <Route path='/profile' element={ <Profile/> } />
+      <Route path='/sign-in' element={ <Signin/> } />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={ <About/>} />
+    </Routes>
+  
+  </BrowserRouter>
 }

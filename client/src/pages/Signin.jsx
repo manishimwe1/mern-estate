a6423c3083f00fadd73efcde7e60a54c537e8484
manishimwe1 <emmymanishimwem@gmail.com>
2023-11-16 17:14:57 +0200

    create sigin page

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import {useState} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
 
-const Signin = () => {
+const SignIn = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      
+      setLoading(true)
+      const res = await fetch('/api/auth/signin ', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json()
+      if (data.success === false) {
+        setError(data.message)
+        setLoading(false)
+        return
+      }
+      setLoading(false);
+      setError(null)
+      navigate('/')
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+  }
+
   return (
-    <div>Signin</div>
+    <div className=' p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className=' flex flex-col gap-4'>
+        <input type='email' placeholder='email ' className='border p-3 rounded-lg ' id='email' onChange={handleChange}/>
+        <input type='password' placeholder='username ' className='border p-3 rounded-lg ' id='password'  onChange={handleChange}/>
+        <button 
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80 '>
+          {loading ? 'Loading...' : 'Sign in'}
+        </button>
+        <div>
+          <Link to={'/sign-up'}>
+            <p className=' text-gray-600'>Dont have an account <span className='text-blue-700 hover:underline underline-offset-4'>Sign up</span></p>
+          </Link>
+        </div>
+        {error && <p className='text-red-500 text-sm mt-5'>{error}</p>}
+      </form>  
+    </div>
   )
 }
 
-export default Signin
+export default SignIn
